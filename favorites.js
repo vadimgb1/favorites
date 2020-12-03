fdf3c3e688eee0afcc42b6719dcec5cf5f15e6cd
Баба Яга <yaga@les.ru>
2020-12-03 07:07:07 +0000

    import from csv

---
+++ favorites.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const csv = require('csv-parser')
+
+const fsStream = fs.createReadStream('favorites.csv')
+const csvStream = csv()
+csvStream.on('data', data =>
+{
+    console.log(data['название'])
+})
+csvStream.on('end', ()=>{console.log("Это конец.")})
+
+fsStream.pipe(csvStream)
